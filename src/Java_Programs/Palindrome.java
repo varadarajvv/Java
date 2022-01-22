129f6a36bc92ee1fed33db446bdc42b653c26c2b
Varadaraj <varadaraj.vv8@gmail.com>
2022-01-22 08:35:14 +0530

    Updated Code

--- src/Java_Programs/Palindrome.java
+++ src/Java_Programs/Palindrome.java
@@ -6,7 +6,7 @@ public class Palindrome {
 	// Compare Reversed String with the Original String
 	public static void main(String args[]) {
 
-		String original = "GADAG";
+		String original = "MADAM";
 		String reverse = ""; // Empty String
 
 		for (int i = original.length() - 1; i >= 0; i--) {

f5b15afcaa00662c49836f81fc901bbc052344fe
Varadaraj <varadaraj.vv8@gmail.com>
2022-01-22 08:28:42 +0530

    First Commit

---
+++ src/Java_Programs/Palindrome.java
@@ -0,0 +1,22 @@
+package Java_Programs;
+
+public class Palindrome {
+	// Identify the Length of String to be Iterated
+	// Reverse the given input String
+	// Compare Reversed String with the Original String
+	public static void main(String args[]) {
+
+		String original = "GADAG";
+		String reverse = ""; // Empty String
+
+		for (int i = original.length() - 1; i >= 0; i--) {
+			reverse = reverse + original.charAt(i);
+		}
+
+		if (original.equals(reverse)){
+			System.out.println(original + ": It's a Palindrome.");
+		} else{
+		System.out.println(original + ": It's not a Palindrome.");
+		}
+	}
+}
